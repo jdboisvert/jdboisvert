82a1d9ef1fe887392c645477aec483a759e93b09
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-03-12 22:04:43 -0400

    Basic working card that goes over some key points I would like to show.

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,45 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/Delta456/box-cli-maker/v2"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jdboisvert",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A personal card for Jeffrey Boisvert (@jdboisvert)",
+	Long:  "jdboisvert is a personal card for Jeffrey Boisvert (@jdboisvert). This is a CLI application helps you to get to know me a little better.",
+	Run: func(cmd *cobra.Command, args []string) {
+		Box := box.New(box.Config{Px: 2, Py: 5, Type: "Double", Color: "Green", ContentAlign: "Center"})
+		Box.Print(
+			"Jeffrey Boisvert (@jdboisvert)",
+			"Current Job: Staff Software Developer at Newton Crypto\n"+
+				"\n\n"+
+				"The Socials:\n"+
+				"Linkedin: https://www.linkedin.com/in/jeffreybv/\n"+
+				"DEV: https://dev.to/jdvert\n"+
+				"\n\n"+
+				"The Code/Projects:\n"+
+				"PyPi: https://pypi.org/user/jdboisvert/\n"+
+				"GitHub: https://github.com/jdboisvert\n"+
+				"\n\n"+
+				"As the famous Alan Turing once said: \n"+
+				"\"We can only see a short distance ahead, but\n"+
+				" we can see plenty there that needs to be done.\""+
+				"\n\n"+
+				"I believe there is always something to improve on and build upon.\n"+
+				"I am always looking for new challenges \n"+
+				"and opportunities to learn and grow."+
+				"\n\n"+
+				"Thanks for checking out my card! Happy coding!",
+		)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +62,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
